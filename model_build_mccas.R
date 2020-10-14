6c6697aaad662218685ecc8bec29ae5327244051
Hanna McCaslin <mccaslinhm@gmail.com>
2020-10-14 17:03:45 -0600

    update

--- model_build_mccas.R
+++ model_build_mccas.R
@@ -8,6 +8,9 @@ plot(log(Price))
 # Correlation matrix for quant vars 
 cor(toyota.df[,c("Price", "Age", "KM", "HP", "CC", "QuartTax", "Weight")])
 
+fit0 <- lm(Price ~ Age + KM + Weight + HP + QuartTax)
+plot(fit0) # def no
+
 fit1 <- lm(log(Price) ~ Age)
 summary(fit1)
 plot(log(Price) ~ Age)
@@ -31,15 +34,15 @@ plot(fit5)
 
 resid5 <- resid(fit5) 
 # look at resids v predictors to think about transformations
-par(mfrow=c(3,2))
-par(mar=c(4, 4, 2, 0.5))
-plot(Age,resid5,xlab="Age",ylab="residual")
+#par(mfrow=c(3,2))
+#par(mar=c(4, 4, 2, 0.5))
+plot(Age,resid5,xlab="Age",ylab="residual") #alright, maybe not totally const var
 abline(a=0,b=0)
-plot(KM,resid5,xlab="KM",ylab="residual")
+plot(KM,resid5,xlab="KM",ylab="residual") #reasonably const var
 abline(a=0,b=0)
-plot(Weight,resid5,xlab="Weight",ylab="residual") # some concern on the right
+plot(Weight,resid5,xlab="Weight",ylab="residual") # some concern on the right (outliers)
 abline(a=0,b=0)
-plot(HP,resid5,xlab="HP",ylab="residual") # some concern on the right
+plot(HP,resid5,xlab="HP",ylab="residual") # some concern on the right (outliers)
 abline(a=0,b=0)
 plot(QuartTax,resid5,xlab="QuartTax",ylab="residual")
 abline(a=0,b=0)
@@ -53,23 +56,27 @@ plot(fit5.5) #this has issues
 
 # Transform predictors
 toyota.df$HP.trans <- HP^(-1/3)
-toyota.df$Weight.trans <- Weight^-4
+toyota.df$Weight.trans <- Weight^-4 #don't know this one is wise -> could be the convergence problem in powerTransform
 attach(toyota.df)
 
 
 fit6 <- lm(log(Price) ~ Age + KM + Weight.trans + HP.trans + QuartTax + Fuel_Type)  
 summary(fit6)
 
-fit7 <- lm(log(Price) ~ Age + KM + Weight.trans + HP.trans + QuartTax + Fuel_Type + Guarantee)  
+fit6.5 <- lm(log(Price) ~ Age + KM + Weight + HP.trans + QuartTax + Fuel_Type)  
+summary(fit6.5)
+
+
+fit7 <- lm(log(Price) ~ Age + KM + Weight + HP.trans + QuartTax + Fuel_Type + Guarantee)  
 summary(fit7) 
 plot(fit7)
-vif(fit7)
+vif(fit7) #fuel_type VIF high
 
-fit8 <- lm(log(Price) ~ Age + KM + Weight.trans + HP.trans + QuartTax + Guarantee)  
-summary(fit8) 
+fit8 <- lm(log(Price) ~ Age + KM + Weight + HP.trans + QuartTax + Guarantee)  
+summary(fit8)  # diagnostics on this one look pretty good
 vif(fit8)
 
-fit9 <- lm(log(Price) ~ Age + KM + Weight.trans + HP.trans + Fuel_Type + Guarantee)  
+fit9 <- lm(log(Price) ~ Age + KM + Weight + HP.trans + Fuel_Type + Guarantee)  
 summary(fit9)  #not as useful as 8
 
 resid8 <- resid(fit8)
@@ -86,8 +93,8 @@ abline(a=0,b=0)
 plot(QuartTax,resid8,xlab="QuartTax",ylab="residual")
 abline(a=0,b=0)
 
-fit10 <- lm(log(Price) ~ Age + KM + Weight.trans + HP.trans + QuartTax*Fuel_Type2 + Guarantee)  
+fit10 <- lm(log(Price) ~ Age + KM + Weight + HP.trans + QuartTax + CC + Fuel_Type2 + QuartTax:Fuel_Type2 + Guarantee)  
 summary(fit10)
 vif(fit10)
 par(mfrow=c(2,2))
-plot(fit10) #qq tails are an issue
+plot(fit10) #qq tails are an issue, std resids don't look ideal, but it's not horrible
